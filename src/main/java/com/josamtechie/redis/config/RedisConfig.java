0dbedeef8cf9031dc76bbe46ed15d49c31f6ce8a
JOMON SAMUEL THOMAS <josamvkd@gmail.com>
2026-04-03 06:24:16 +0400

    Third commit removed Unused RedisTemplate config from config file
    Removed Unused RedisTemplate config from config file

--- src/main/java/com/josamtechie/redis/config/RedisConfig.java
+++ src/main/java/com/josamtechie/redis/config/RedisConfig.java
@@ -5,10 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
@@ -26,19 +23,5 @@ public class RedisConfig
         configuration.setHostName(redisHost);
         configuration.setPort(redisPort);
         return new JedisConnectionFactory(configuration);
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate()
-    {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new JdkSerializationRedisSerializer());
-        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
-        redisTemplate.setEnableTransactionSupport(true);
-        redisTemplate.afterPropertiesSet();
-        return redisTemplate;
     }
 }
